18bfea9a6e54a86910dcf96dd8d0470ac798d9c9
romanti-ezer <romanti.santos@fatec.sp.gov.br>
2018-04-03 22:14:26 -0300

    Implemented seach suggestions in the template.

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -14,14 +14,15 @@ import { Subject } from 'rxjs/Subject';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>
+  public ofertas2: Oferta[]
   private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
     this.ofertas = this.subjectPesquisa
-      .debounceTime(1000)
-      .distinctUntilChanged()
+      .debounceTime(1000) // Para executar a ação do switchMap após 1 segundo
+      .distinctUntilChanged() // Para fazer pesquisas distintas
       .switchMap((termo: string) => {
         if (termo === '') {
           return Observable.of<Oferta[]>([])
@@ -33,7 +34,9 @@ export class TopoComponent implements OnInit {
         console.log(err)
         return Observable.of<Oferta[]>([])
       })
-    this.ofertas.subscribe((ofertas: Oferta[]) => console.log(ofertas))
+    this.ofertas.subscribe((ofertas: Oferta[]) => {
+      this.ofertas2 = ofertas
+    })
   }
 
   public pesquisa(termoDaBusca: string): void {
